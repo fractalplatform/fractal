291de89d4cd2488dab9408e987527da14be86004
GeekCat99 <zhihongzhang_99@126.com>
2019-03-13 10:50:35 +0800

    format code

--- utils/trie/trie.go
+++ utils/trie/trie.go
@@ -1,18 +1,18 @@
-// Copyright 2018 The zipper team Authors
-// This file is part of the z0 library.
+// Copyright 2018 The Fractal Team Authors
+// This file is part of the fractal project.
 //
-// The z0 library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The z0 library is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
+// GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License
-// along with the z0 library. If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 // Package trie implements Merkle Patricia Tries.
 package trie
