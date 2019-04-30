976305ba2be25964e5723f42452766988f3fba6b
GeekRes <42427073+GeekRes@users.noreply.github.com>
2019-04-30 15:47:06 +0800

    disbale other account create subaccount (#205)
    * disbale other account create subaccount
    
    * add snapshot test

--- snapshot/snapshot_test.go
+++ snapshot/snapshot_test.go
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 package snapshot
 
 import (
@@ -31,20 +32,24 @@ func TestSnapshot(t *testing.T) {
 	cachedb := state.NewDatabase(db)
 	prevHash := common.Hash{}
 	state1, _ := state.New(prevHash, cachedb)
+
 	addr := "snapshot01"
 	key := "aaaaaa"
 	value := []byte("1")
 	state1.Put(addr, key, value)
+
 	root, err := state1.Commit(batch, prevHash, 0)
 	if err != nil {
 		t.Error("commit trie err", err)
 	}
+
 	triedb := state1.Database().TrieDB()
 	triedb.Reference(root, common.Hash{})
 	if err := triedb.Commit(root, false); err != nil {
 		t.Error("commit db err", err)
 	}
 	triedb.Dereference(root)
+
 	state2, _ := state.New(root, cachedb)
 	snapshotManager := NewSnapshotManager(state2)
 	err = snapshotManager.SetSnapshot(uint64(100000000), BlockInfo{Number: 0, BlockHash: prevHash, Timestamp: 0})
@@ -59,21 +64,26 @@ func TestSnapshot(t *testing.T) {
 		BlockHash: prevHash,
 	}
 	rawdb.WriteSnapshot(db, key1, snapshotInfo)
+
 	timestamp, err := snapshotManager.GetLastSnapshotTime()
 	if err != nil {
 		t.Error("set snapshot err", err)
 	}
+
 	if timestamp != 100000000 {
 		t.Error("set snapshot err", err)
 	}
+
 	timestamp, err = snapshotManager.GetPrevSnapshotTime(100000000)
 	if err != nil {
 		t.Error("set snapshot err", err)
 	}
+
 	_, _, err = snapshotManager.GetCurrentSnapshotHash()
 	if err != nil {
 		t.Error("set snapshot err", err)
 	}
+
 	_, err = snapshotManager.GetSnapshotMsg(addr, key, 100000000)
 	if err != nil {
 		t.Error("set snapshot err", err)
