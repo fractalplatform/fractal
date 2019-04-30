74562eb79924743deb29654fd161e891cbadac6b
lixp <894646171@qq.com>
2019-04-30 16:31:52 +0800

    txpool: add some test case

--- txpool/handler_test.go
+++ txpool/handler_test.go
@@ -2,9 +2,17 @@ package txpool
 
 import (
 	"fmt"
+	"math/big"
 	"testing"
 
+	am "github.com/fractalplatform/fractal/accountmanager"
+	"github.com/fractalplatform/fractal/asset"
+	"github.com/fractalplatform/fractal/common"
+	"github.com/fractalplatform/fractal/event"
+	"github.com/fractalplatform/fractal/params"
+	"github.com/fractalplatform/fractal/state"
 	"github.com/fractalplatform/fractal/types"
+	mdb "github.com/fractalplatform/fractal/utils/fdb/memdb"
 )
 
 const (
@@ -79,5 +87,82 @@ func TestBloom(t *testing.T) {
 				t.Fatalf("%d %x %x\n", i, hadTx.Hash(), *cache.getTxBloom(hadTx))
 			}
 		}
+	}
+}
+
+func TestP2PTxMsg(t *testing.T) {
+	var (
+		statedb, _ = state.New(common.Hash{}, state.NewDatabase(mdb.NewMemDatabase()))
+		manager, _ = am.NewAccountManager(statedb)
+		fname      = common.Name("fromname")
+		tname      = common.Name("totestname")
+		fkey       = generateAccount(t, fname, manager)
+		_          = generateAccount(t, tname, manager)
+		asset      = asset.NewAsset(statedb)
+		trigger    = false
+	)
+
+	// issue asset
+	if err := asset.IssueAsset("ft", 0, "zz", new(big.Int).SetUint64(params.Fractal), 10, common.Name(""), fname, new(big.Int).SetUint64(params.Fractal), common.Name(""), ""); err != nil {
+		t.Fatal(err)
+	}
+
+	// add balance
+	if err := manager.AddAccountBalanceByName(fname, "ft", new(big.Int).SetUint64(params.Fractal)); err != nil {
+		t.Fatal(err)
+	}
+
+	blockchain := &testChain{&testBlockChain{statedb, 1000000000, new(event.Feed)}, fname, &trigger}
+
+	pool := New(testTxPoolConfig, params.DefaultChainconfig, blockchain)
+	defer pool.Stop()
+
+	nonce, err := pool.State().GetNonce(fname)
+	if err != nil {
+		t.Fatal("Invalid getNonce ", err)
+	}
+	if nonce != 0 {
+		t.Fatalf("Invalid nonce, want 0, got %d", nonce)
+	}
+
+	txs := []*TransactionWithPath{
+		&TransactionWithPath{
+			Tx:    transaction(0, fname, tname, 100000, fkey),
+			Bloom: &types.Bloom{},
+		},
+		&TransactionWithPath{
+			Tx:    transaction(1, fname, tname, 100000, fkey),
+			Bloom: &types.Bloom{},
+		},
+	}
+	event.SendTo(event.NewLocalStation("test", nil), nil, event.P2PTxMsg, txs)
+	for {
+		if pending, quened := pool.Stats(); pending > 0 || quened > 0 {
+			break
+		}
+	}
+
+	nonce, err = pool.State().GetNonce(fname)
+	if err != nil {
+		t.Fatal("Invalid getNonce ", err)
+	}
+	if nonce != 2 {
+		t.Fatalf("Invalid nonce, want 2, got %d", nonce)
+	}
+
+	// trigger state change in the background
+	trigger = true
+	pool.lockedReset(nil, nil)
+
+	_, err = pool.Pending()
+	if err != nil {
+		t.Fatalf("Could not fetch pending transactions: %v", err)
+	}
+	nonce, err = pool.State().GetNonce(fname)
+	if err != nil {
+		t.Fatal("Invalid getNonce ", err)
+	}
+	if nonce != 2 {
+		t.Fatalf("Invalid nonce, want 2, got %d", nonce)
 	}
 }
