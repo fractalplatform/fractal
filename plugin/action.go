89ed08780903d7797cb42db101de705fb1dcd412
jacobdenver007 <jacobdenver@163.com>
2019-10-29 11:00:22 +0800

    add action.go

---
+++ plugin/action.go
@@ -0,0 +1,43 @@
+package plugin
+
+import (
+	"math/big"
+
+	"github.com/fractalplatform/fractal/common"
+	"github.com/fractalplatform/fractal/types"
+)
+
+const (
+	// CreateAccount repesents the create account.
+	CreateAccount types.ActionType = 0x100 + iota
+)
+
+const (
+	// CreateAccount repesents the create account.
+	IssueAsset types.ActionType = 0x200 + iota
+	IncreaseAsset
+)
+
+type CreateAccountAction struct {
+	Name   string
+	Pubkey common.PubKey
+	Desc   string
+}
+
+type AddAssetAction struct {
+	AssetID uint64
+	Amount  *big.Int
+	To      string
+}
+
+type IssueAssetAction struct {
+	AssetName   string
+	Symbol      string
+	Amount      *big.Int
+	Owner       string
+	Founder     string
+	Decimals    uint64
+	UpperLimit  *big.Int
+	Contract    string
+	Description string
+}
