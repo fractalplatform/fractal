27f3addabbf6caecddc03f9c4e1d086c7d1c6bd9
erick yan <46879318+erickyan86@users.noreply.github.com>
2019-12-16 19:48:00 +0800

    packing insufficient balance tx

--- processor/transition.go
+++ processor/transition.go
@@ -133,7 +133,7 @@ func (st *StateTransition) TransitionDb() (ret []byte, usedGas uint64, failed bo
 		// The only possible consensus-error would be if there wasn't
 		// sufficient balance to make the transfer happen. The first
 		// balance transfer may never fail.
-		if vmerr == plugin.ErrInsufficientBalance || vmerr == vm.ErrInsufficientBalance || vmerr == vm.ErrExecOverTime {
+		if vmerr == vm.ErrExecOverTime {
 			return nil, 0, false, vmerr, vmerr
 		}
 	}
